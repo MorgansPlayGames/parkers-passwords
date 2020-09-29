85563d4793b746d59cba3900664b585912e1d178
Parker Morgan <jpmaster13@msn.com>
2020-09-29 15:12:32 -0600

    Added the check to make the total characters array and started the generate password for loop

--- script.js
+++ script.js
@@ -14,16 +14,42 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
+  //constants
   var capitalList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCaseList = "abcdefghijklmnopqrstuvwxyz";
+  var lowercaseList = "abcdefghijklmnopqrstuvwxyz";
   var numberList = "1234567890";
   var specialCharacterList = "`~!@#$%^&*()_-=+[]{}|;':\",.<>?"
-  
+
+  //Password characters allowed
+  var totalCharacters = "";
+
+  //User input
   var pwLength = prompt("How many characters would you like your password? (number between 8 and 128)");
   var capitalCheck = confirm("Do you want capital letters in your password?");
   var lowercaseCheck = confirm("Do you want lowercase characters in your password?");
   var numberCheck = confirm("Do you want numbers in your password?");
   var specialCheck = confirm("Do you want special characters in your password?");
 
+  var indexNumber = "";
+  var characterAdder = "";
+  //Set the total characters used
+  if(capitalCheck === true){
+    totalCharacters = totalCharacters + capitalList;
+  }
+  if(lowercaseCheck === true){
+    totalCharacters = totalCharacters + lowercaseList;
+  }
+  if(numberCheck === true){
+    totalCharacters = totalCharacters + numberList;
+  }
+  if(specialCheck === true){
+    totalCharacters = totalCharacters + specialCharacterList;
+  }
 
+  console.log(totalCharacters);
+
+  for(var i = pwLength; i > 0; i--){
+
+   }
+   return(totalCharacters);
 }
