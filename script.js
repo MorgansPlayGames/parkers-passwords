044c681011ccd773eb06ea0d34966da6496e8d1c
Parker Morgan <jpmaster13@msn.com>
2020-09-29 14:45:17 -0600

    Added the generate password function and created all the variables needed for input within.

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+generatePassword(){
+  var capitalList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCaseList = "abcdefghijklmnopqrstuvwxyz";
+  var numberList = "1234567890";
+  var specialCharacterList = "`~!@#$%^&*()_-=+[]{}|;':\",.<>?"
+  
+  var pwLength = prompt("How many characters would you like your password? (number between 8 and 128)");
+  var capitalCheck = confirm("Do you want capital letters in your password?");
+  var lowercaseCheck = confirm("Do you want lowercase characters in your password?");
+  var numberCheck = confirm("Do you want numbers in your password?");
+  var specialCheck = confirm("Do you want special characters in your password?");
+  
+
+}
